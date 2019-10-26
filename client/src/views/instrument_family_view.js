ddb0d07082c8b8dd977d86c4a32398439d2116b0
denkoroku <cathriddoch@gmail.com>
2019-10-26 14:17:25 +0100

    changes import-export stuff

--- client/src/views/instrument_family_view.js
+++ client/src/views/instrument_family_view.js
@@ -1,4 +1,4 @@
-const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
 
 class InstrumentFamilyView{
   constructor(container) {
@@ -45,4 +45,5 @@ class InstrumentFamilyView{
     return list;
   }
 }
-module.exports = InstrumentFamilyView;
+
+export default InstrumentFamilyView;
